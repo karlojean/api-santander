39c2326e60516095ba39cedf2687eadb9a44a49f
karlojean <jeankarlodev@gmail.com>
2024-05-23 00:13:03 -0300

    Start project

---
+++ src/test/java/dio/apisantander/ApiSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.apisantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
